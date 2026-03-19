5305a5e91abc363d129f45eb76fc1edcd9bf2041
Rami <rami.elsaneh@protonmail.com>
2026-03-19 15:51:23 +0100

    info design and layout

--- mitt-projekt/src/App.jsx
+++ mitt-projekt/src/App.jsx
@@ -1,8 +1,8 @@
 import Info from "./Info"
 export default function App() {
 	return (
-		<>
+		<div className="app-container">
 			<Info />
-		</>
+		</div>
 	)
 }
--- mitt-projekt/src/Info.jsx
+++ mitt-projekt/src/Info.jsx
@@ -1,12 +1,12 @@
 import ramiIgm from "./assets/rami.png"
 export default function Info() {
 	return (
-		<div>
-			<img src={ramiIgm} alt="image of Rami El-saneh" width="200px"/>
+		<div className="card_info">
+			<img src={ramiIgm} alt="image of Rami El-saneh" width="100px"/>
 			<h1>
 				Rami El-saneh
 			</h1>
-			<p>
+			<p className ="card_title">
 				Frontend Developer and founder of coffee code studio
 			</p>
 			<a href="https://coffeecodestudio.se">
