cd465911c600ea77133daec0a178351db6296b34
Rami <rami.elsaneh@protonmail.com>
2026-03-23 13:03:18 +0100

    body removec margin and apdding

--- mitt-projekt/src/App.jsx
+++ mitt-projekt/src/App.jsx
@@ -5,11 +5,15 @@ import Footer from "./Footer.jsx"
 
 export default function App() {
 	return (
+		<>
 		<div className="app-container">
 			<Info />
 			<About />
 			<Interest />
 			<Footer />
 		</div>
+		
+		
+		</>
 	)
 }
