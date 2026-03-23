4e474137fd8545a8253d4857b09e5b852e94c589
Rami <rami.elsaneh@protonmail.com>
2026-03-23 12:42:35 +0100

    design uppgrade, added footer

--- mitt-projekt/src/Footer.jsx
+++ mitt-projekt/src/Footer.jsx
@@ -1,26 +1,14 @@
-import { FaLinkedin, FaGithub } from 'react-icons/fa'; // Lägg till denna!
-import { SiNetlify } from 'react-icons/si';           // Lägg till denna!
-
 export default function Footer() {
-  return (
-    <footer className="footer">
-      <div className="footer-icons">
-        
-        {/* LinkedIn - Glöm inte att lägga till denna länk också om du vill ha den */}
-        <a href="https://linkedin.com/in/DITT-NAMN" target="_blank" rel="noreferrer" aria-label="LinkedIn">
-          <FaLinkedin size={30} />
-        </a>
+	return (
+	
+			<footer class="footer">
+  				<a href="https://www.linkedin.com/in/rami-e-453b77330/" target="_blank" class="icon linkedin">
+    		<i class="fab fa-linkedin-in"></i>
+  				</a>
+  <a href="https://github.com/CoffeeCodeStudio" target="_blank" class="icon github">
+    <i class="fab fa-github"></i>
+  </a>
 
-        <a href="https://github.com/DITT-ANVÄNDARNAMN" target="_blank" rel="noreferrer" aria-label="GitHub">
-          <FaGithub size={30} />
-        </a>
-
-        <a href="https://app.netlify.com/teams/DITT-NAMN" target="_blank" rel="noreferrer" aria-label="Netlify">
-          <SiNetlify size={30} />
-        </a>
-
-      </div>
-      <p>&copy; 2026 Rami</p>
-    </footer>
-  );
+		</footer>
+	)
 }
--- mitt-projekt/src/Info.jsx
+++ mitt-projekt/src/Info.jsx
@@ -2,6 +2,7 @@ import ramiIgm from "./assets/rami.png"
 export default function Info() {
 	return (
 		<div className="card_info">
+			
 			<img src={ramiIgm} alt="image of Rami El-saneh" width="100px"/>
 			<h1>
 				Rami El-saneh
@@ -10,15 +11,14 @@ export default function Info() {
 				Frontend Developer and founder of coffee code studio
 			</p>
 			<a href="https://coffeecodestudio.se">
-			Coffee Code Studio
+			<p>☕ Coffee Code Studio</p>
 			</a>
 			
+			<div class="btn">
 				<button>
-					Email
+				✉️	Email
 				</button>
-				<button>
-					linkedin
-				</button>
+			</div>
 			
 		</div>
 	)
