f57bad9f8e8dd373da96475989043b8ba22bf1f3
Rami <rami.elsaneh@protonmail.com>
2026-03-23 13:20:12 +0100

    Publicerar bygget

--- mitt-projekt/src/Interest.jsx
+++ mitt-projekt/src/Interest.jsx
@@ -2,7 +2,7 @@ export default function About() {
 	return (
 		<div>
 			<h2>
-				Interest
+				Interests
 			</h2>
 			<p>
 				På fritiden är jag intresserad av teknik, webbutveckling och att lära mig nya verktyg och lösningar. Jag gillar också att förstå hur saker fungerar bakom kulisserna och att förbättra mina färdigheter steg för steg.
